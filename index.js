b8bac6be0def7a65745525101c4866982ba3b845
Rohit Kotian <rohitkotian07@gmail.com>
2024-10-17 21:19:51 +0530

    CORS fixes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://askjarvis-backend.vercel.app",
+    origin: "https://askjarvis.vercel.app/",
     methods: ["POST", "GET"],
     credentials: true,
   })
