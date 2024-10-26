95fdd2f63c9c0a9858d30204b15431a6d1766ed4
Rohit Kotian <rohitkotian07@gmail.com>
2024-10-26 10:34:56 +0530

    DB Schema updates

--- models/Users.js
+++ models/Users.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
 
+const chatSchema = new mongoose.Schema({
+  text: Array,
+  isBot: Boolean,
+  skipCopy: Boolean,
+  isLoading: Boolean,
+});
+
 const UserSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
+  userChats: [chatSchema],
 });
 
 const UserModel = mongoose.model("Users", UserSchema);
